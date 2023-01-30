7f13a9e1e4aff77ff09d08d1ed9d79d2b5e84c02
chilakamarthhyb <123915753+chilakamarthhyb@users.noreply.github.com>
2023-01-29 21:06:41 -0600

    Update streamlit_app.py
    Fixed format 3

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Mom\'s New Healthy Diner')
 streamlit.header('Breakfast Favorites')
-streamlit.text(🥣 'Omega 3 & Buleberry Oatmeal')
-streamlit.text(🥗'Kale, Spinach & Racket Smoothie')
-streamlit.text(🐔'Hard boiled Free-Range Egg')
-streamlit.text(🥑🍞'Avocado Toast')
+streamlit.text('🥣 Omega 3 & Buleberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Racket Smoothie')
+streamlit.text('🐔Hard boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
